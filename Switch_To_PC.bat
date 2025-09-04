ea3b389da9003bee2ccf1283c54d5ecb2c7588f6
Shadowedvaca <shadowedvaca@gmail.com>
2025-09-04 14:33:22 -0700

    monitor name diff on laptop

--- Switch_To_PC.bat
+++ Switch_To_PC.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Switching TV back to PC Mode...
-C:\controlmymonitor\ControlMyMonitor.exe /SetValue "40C1R" 60 5
+C:\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 5
 echo Switched TV to PC Mode
