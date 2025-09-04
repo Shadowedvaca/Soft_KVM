75b6a60436c36727adad8e362a815519553e4320
Shadowedvaca <shadowedvaca@gmail.com>
2025-09-04 11:36:24 -0700

    made bat files

---
+++ Switch_To_Laptop.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Switching TV to Laptop Mode...
+C:\controlmymonitor\ControlMyMonitor.exe /SetValue "40C1R" 60 8
+echo Switched TV to Laptop Mode
+echo Note: Run the switch script from your laptop to switch back
---
+++ Switch_To_PC.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Switching TV back to PC Mode...
+C:\controlmymonitor\ControlMyMonitor.exe /SetValue "40C1R" 60 5
+echo Switched TV to PC Mode
